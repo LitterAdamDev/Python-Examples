62f33721914d09fe0eeb02e37fd1b34f909b936d
LitterAdamDev <adamlitter99@gmail.com>
2024-01-28 23:58:54 +0100

    numpy example

--- numba_example/main.py
+++ numba_example/main.py
@@ -2,6 +2,7 @@ from numba import jit
 import random
 import math
 import time
+import numpy as np
 
 @jit(nopython=True)
 def do_some_math(n):
@@ -12,8 +13,18 @@ def do_some_math(n):
         z += math.sqrt(x ** 2 + y ** 2)
     return z
 
+@jit(nopython=True)
+def do_some_math_numpy(n):
+    z = np.zeros((n, n))
+    for i in range(n):
+        x = np.random.rand(n, n)
+        y = np.random.rand(n, n)
+        z += np.sqrt(x ** 2 + y ** 2)
+    return z 
+
 start = time.time()
-do_some_math(1_000_000_000)
+#do_some_math(1_000_000_000)
+do_some_math_numpy(1000)
 end = time.time()
 
 print(end - start)
