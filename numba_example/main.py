635b983bb0161d63a81756abeca38e1ba8e89f60
LitterAdamDev <adamlitter99@gmail.com>
2024-01-29 11:27:18 +0100

    req update

--- numba_example/main.py
+++ numba_example/main.py
@@ -3,6 +3,8 @@ import random
 import math
 import time
 import numpy as np
+import pandas as pd
+import pandas_datareader as pdr
 
 @jit(nopython=True)
 def do_some_math(n):
@@ -22,9 +24,19 @@ def do_some_math_numpy(n):
         z += np.sqrt(x ** 2 + y ** 2)
     return z 
 
+def pandas_function(data : pd.DataFrame):
+    result = data.sort_values(by=['Volume'])
+    result = result.applymap(math.sqrt)
+    result += 2
+    result = result.applymap(lambda x: x ** 2)
+    result = result.T
+    return data #result
+
 start = time.time()
 #do_some_math(1_000_000_000)
-do_some_math_numpy(1000)
+#do_some_math_numpy(1000)
+data = pdr.DataReader("AAPL", "yahoo")
+print(data)
 end = time.time()
 
 print(end - start)
